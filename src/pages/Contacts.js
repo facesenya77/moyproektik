37d5dc0a7174dd66d9808388cb9346dd78461849
Arseniy <facesenya@mail.ru>
2024-05-20 20:16:04 +0300

    переделал страницу контактов

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,35 +1,40 @@
 import React from 'react';
+import { FaTelegram, FaWhatsapp, FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 const Contacts = () => {
     const openTelegramChat = () => {
         window.open('https://t.me/ssssnya', '_blank');
     };
 
+    const openWhatsappChat = () => {
+        window.open('https://wa.me/79234843779', '_blank');
+    };
+
+    const openFacebook = () => {
+        window.open('https://www.facebook.com/example', '_blank');
+    };
+
+    const openInstagram = () => {
+        window.open('https://www.instagram.com/example', '_blank');
+    };
+
+    const openTwitter = () => {
+        window.open('https://twitter.com/example', '_blank');
+    };
+
     return (
         <main className="section">
             <div className="container">
                 <h1 className="title-1">Контакты</h1>
+                <h2 className="otstup">Если остались вопросы:</h2>
 
-                <ul className="content-list">
-                    <li className="content-list__item">
-                        <h2 className="title-2">Местоположение</h2>
-                        <p className="sochi">г. Сочи</p>
-                    </li>
-                    <li className="content-list__item">
-                        <h2 className="title-2">Telegram / WhatsApp</h2>
-                        <p>
-                            <a className='sochi' href="tel:+79234843779" onClick={openTelegramChat}>+7 (923) 484-37-79</a>
-                        </p>
-                    </li>
-                    <li className="content-list__item">
-                        <h2 className="title-2">Email</h2>
-                        <p>
-                            <a className='sochi' href="mailto:arseniyshag762@gmail.com">
-                                arseniyshag762@gmail.com
-                            </a>
-                        </p>
-                    </li>
-                </ul>
+                <div className="social-icons-container">
+                    <FaTelegram className="icon" onClick={openTelegramChat} />
+                    <FaWhatsapp className="icon" onClick={openWhatsappChat} />
+                    <FaFacebook className="icon" onClick={openFacebook} />
+                    <FaInstagram className="icon" onClick={openInstagram} />
+                    <FaTwitter className="icon" onClick={openTwitter} />
+                </div>
             </div>
         </main>
     );
