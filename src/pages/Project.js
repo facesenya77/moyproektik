40699836dab1ed0bed94d21a245a4bb015863b03
Arseniy <facesenya@mail.ru>
2024-05-23 20:28:41 +0300

    сделал анимации и изменил карточки проектов

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,33 +1,66 @@
 import {useParams} from "react-router-dom";
 import BtnGitHub from "../components/btnGitHub/BtnGitHub";
 import {projects} from "./../helpers/projectsList"
+import {motion} from 'framer-motion';
+
+const textAnimation = {
+	hidden: {
+	  y: 100,
+	  opacity: 0,
+	},
+	visible: custom => ({
+	  y: 0,
+	  opacity: 1,
+	  transition: { delay: custom * 0.2 }
+	}),
+  }
 
 const Project = () => {
 	const {id} = useParams();
 	const project = projects[id];
 
     return (
-		<main className="section">
+		<motion.main 
+		initial="hidden"
+			whileInView="visible"
+			viewport={{ amount: 0.2 }}
+			className="section">
 			<div className="container">
 				<div className="project-details">
 					<h1 className="title-1">{project.title}</h1>
 
-					<img
+					<motion.img custom={1} variants={textAnimation}
 						src={project.imgBig}
 						alt={project.title}
 						className="project-details__cover"
 					/>
+					<motion.div custom={2} variants={textAnimation} className="project-details__desc">
+						<p>Коротко о проекте:</p>
+					</motion.div>
+					<div className="symbols">
+					<motion.div custom={3} variants={textAnimation} className="project-details__desc1">
+						<p>✔️Проект написан на: {project.skills}</p>
+						</motion.div>
 
-					<div className="project-details__desc">
-						<p>Язык: {project.skills}</p>
-					</div>
+					<motion.div custom={4} variants={textAnimation} className="project-details__desc1">
+						<p>🕓Проект был разработан за неделю командой из трех разработчиков </p>
+						</motion.div>
 
+					<motion.div custom={5} variants={textAnimation} className="project-details__desc1">
+						<p>⚡Имеет дружественный интерфейс и документацию для легкого использования и понимания </p>
+						</motion.div>
+
+					<motion.div custom={6} variants={textAnimation} className="project-details__desc1">
+						<p>🚀Проект успешно запущен и используется в боевых условиях </p>
+						</motion.div>
+						</div>
 					{project.gitHubLink && (
 						<BtnGitHub link="https://github.com/shsv382" />
 					)}
 				</div>
 			</div>
-		</main>
+			
+		</motion.main>
 	);
 }
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,27 +1,53 @@
 import Project from '../components/project/Project';
-import {projects} from "./../helpers/projectsList"
+import { projects } from "./../helpers/projectsList";
+import { motion, Variants } from "framer-motion";
+
 
 const Projects = () => {
-	return (
-		<main className="section">
-			<div className="container">
-				<h2 className="title-1">Проекты</h2>
-				<ul className="projects">
-					{projects.map((project, index) => {
-						return (
-							<Project
-								key={index}
-								title={project.title}
-								img={project.img}
-								imgURL={project.imgURL}
-								index={index}
-							/>
-						);
-					})}
-				</ul>
-			</div>
-		</main>
-	);
+  const containerVariants: Variants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.5
+      }
+    }
+  };
+
+  const itemVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      y: 20
+    },
+    visible: {
+      opacity: 1,
+      y: 0
+    }
+  };
+
+  const transition = {
+    duration: 0.3 
+  };
+
+  return (
+    <motion.main className="section" initial="hidden" animate="visible" variants={containerVariants}>
+      <div className="container">
+        <motion.h2 className="title-1" variants={itemVariants} transition={transition}>Проекты</motion.h2>
+        <ul className="projects">
+          {projects.map((project, index) => (
+            <motion.li key={index} variants={itemVariants} transition={transition}>
+              <Project
+                title={project.title}
+                img={project.img}
+                imgURL={project.imgURL}
+                index={index}
+              />
+            </motion.li>
+          ))}
+        </ul>
+      </div>
+    </motion.main>
+  );
 };
 
 export default Projects;
+
