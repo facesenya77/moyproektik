2e67a4a079a69ed2465faed9409fa9664dcc32c8
Arseniy <facesenya@mail.ru>
2024-06-03 19:50:45 +0300

    сделал scramble text и поменял faicon

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,42 @@
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 import BtnDarkMode from '../btnDarkMode/BtnDarkMode';
 import './style.css';
+
+const ScrambleText = ({ text, scrambleTime }) => {
+  const [displayedText, setDisplayedText] = useState('');
+
+  useEffect(() => {
+    let intervalId;
+
+    const scramble = () => {
+      return text.split('').map(() => {
+        return String.fromCharCode(33 + Math.random() * (127 - 33));
+      }).join('');
+    };
+
+    const startScrambling = () => {
+      intervalId = setInterval(() => {
+        setDisplayedText(scramble());
+      }, 100); 
+    };
+
+    startScrambling();
+
+    const timeoutId = setTimeout(() => {
+      clearInterval(intervalId); 
+      setDisplayedText(text); 
+    }, scrambleTime);
+
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(timeoutId);
+    };
+  }, [text, scrambleTime]);
+
+  return <motion.span>{displayedText}</motion.span>;
+};
 
 const Navbar = () => {
 	const activeLink = 'nav-list__link nav-list__link--active';
@@ -8,13 +44,13 @@ const Navbar = () => {
 
 	return (
 		<nav className="nav">
-			<div className="container">
-				<div className="nav-row">
-					<NavLink to="/" className="logo">
-						<strong>Фриланс</strong> портфолио
-					</NavLink>
-
-					<BtnDarkMode />
+		  <div className="container">
+			<div className="nav-row">
+			  <NavLink to="/" className="logo">
+				<ScrambleText text="Фриланс портфолио" scrambleTime={1700} />
+			  </NavLink>
+	
+			  <BtnDarkMode />
 
 					<ul className="nav-list">
 						<li className="nav-list__item">
